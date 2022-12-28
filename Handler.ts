0a308be18ec11d65d960efe08db86e8817f7dcf3
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2022-12-28 15:34:37 +0700

    Upgrade all

--- Handler.ts
+++ Handler.ts
@@ -1,12 +1,11 @@
-import { Filter } from '../decimaker/models/Filter'
 import { Step } from './Step'
 import { Transition } from './Transition'
 
 export interface Handler<Params, State> {
-  filter: Filter<Step<Params, State>>
+  filter: (step: Step<Params, State>) => boolean,
   transition: Transition<Params, State>
 }
 
-export function handler<Params, State>(filter: Filter<Step<Params, State>>, transition: Transition<Params, State>): Handler<Params, State> {
+export function handler<Params, State>(filter: (step: Step<Params, State>) => boolean, transition: Transition<Params, State>): Handler<Params, State> {
   return { filter, transition }
 }
