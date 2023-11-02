eaabe676b0bbad1770b7175018742f42f6c77ed8
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2023-11-02 16:17:48 +0700

    Enable exactOptionalPropertyTypes

--- helpers.ts
+++ helpers.ts
@@ -3,7 +3,6 @@ import { GenericState } from './models/GenericState'
 export function err<Data, Output, Error>(state: GenericState<Data, Output, Error>, error: Error): GenericState<Data, Output, Error> {
   return {
     data: state.data,
-    output: undefined,
     error,
   }
 }
